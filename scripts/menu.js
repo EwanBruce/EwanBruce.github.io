0b4f3c7eed7e634709913f8ef4292fcec94dcb72
EBruce <git@ebruce.uk>
2021-02-12 21:24:55 +0000

    -

--- scripts/menu.js
+++ scripts/menu.js
@@ -1,8 +1,7 @@
-function togglediv(id) {
-    var div = document.getElementById(id);
+function togglenav() {
     var w = document.documentElement.clientWidth || document.body.clientWidth || window.innerWidth;
-    var targetWidth = 700;
+    var targetWidth = 600;
     if (w <= targetWidth) {     
-    div.style.display = div.style.display == "none" ? "" : "none";
+    document.getElementById("nav").style.height = document.getElementById("nav").style.height == "auto" ? "60px" : "auto";
     }
 }
