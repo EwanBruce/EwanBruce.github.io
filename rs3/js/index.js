2b963a418bfd3cfdf76d42a62de367fc9e148951
EBruce <git@ebruce.uk>
2021-06-16 21:06:28 +0100

    -

--- rs3/js/index.js
+++ rs3/js/index.js
@@ -1,2 +1,2 @@
-import data from 'https://secure.runescape.com/m=itemdb_rs/api/catalogue/detail.json?item=440';
-document.getElementById('price').innerHTML = `PRICE: ${data.item.current.price}`;
+var ore 'https://secure.runescape.com/m=itemdb_rs/api/catalogue/detail.json?item=440');
+document.getElementById('price').innerHTML = `PRICE: ${ore.item.current.price}`;
