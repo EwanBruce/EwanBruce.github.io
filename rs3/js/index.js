5608dcc5d2f9647db0bc5bba5106bbbb8434c15d
EBruce <git@ebruce.uk>
2021-06-17 01:59:13 +0100

    -

--- rs3/js/index.js
+++ rs3/js/index.js
@@ -1,3 +1,9 @@
-var ore = JSON.parse(data);
-document.getElementById('price').innerHTML = 'hello';
-//document.getElementById('price').innerHTML = `PRICE: ${ore.item.current.price}`;
+const xmlhttp = new XMLHttpRequest();
+xmlhttp.onload = function() {
+  const itemdata = JSON.parse(this.responseText);
+  document.getElementById("price").innerHTML = itemdata.item.current.price;
+}
+xmlhttp.open("GET", "https://secure.runescape.com/m=itemdb_rs/api/catalogue/detail.json?item=440");
+xmlhttp.withCredentials = true;
+//xmlhttp.Access-Control-Allow-Origin=
+xmlhttp.send();
