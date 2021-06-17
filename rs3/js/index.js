4ae7eb8174c9ba2b3af5b75b4acc41b98d7f7b36
EBruce <git@ebruce.uk>
2021-06-17 17:24:40 +0100

    -

--- rs3/js/index.js
+++ rs3/js/index.js
@@ -1,21 +1,22 @@
-function readJson () {
-  // http://localhost:8080
-  fetch('./itemdb.json')
-  .then(response => {
-      if (!response.ok) {
-          throw new Error("HTTP error " + response.status);
-      }
-      return response.json();
-  })
-  .then(json => {
-      this.users = json;
-      //console.log(this.users);
-  })
-  .catch(function () {
-      this.dataError = true;
-  })
+// Example POST method implementation:
+async function postData(url = './itemdb.json', data = {}) {
+  // Default options are marked with *
+    method: 'POST', // *GET, POST, PUT, DELETE, etc.
+    mode: 'cors', // no-cors, *cors, same-origin
+    cache: 'no-cache', // *default, no-cache, reload, force-cache, only-if-cached
+    credentials: 'same-origin', // include, *same-origin, omit
+    headers: {
+      'Content-Type': 'application/json'
+      // 'Content-Type': 'application/x-www-form-urlencoded',
+    },
+    redirect: 'follow', // manual, *follow, error
+    referrerPolicy: 'no-referrer', // no-referrer, *no-referrer-when-downgrade, origin, origin-when-cross-origin, same-origin, strict-origin, strict-origin-when-cross-origin, unsafe-url
+    body: JSON.stringify(data) // body data type must match "Content-Type" header
+  });
+  return response.json(); // parses JSON response into native JavaScript objects
 }
 
-var items = readJson();
-
-console.log(items.item1.price);
+postData('./itemdb.json', { answer: 42 })
+  .then(data => {
+    console.log(data); // JSON data parsed by `data.json()` call
+  });
