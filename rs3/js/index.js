f911572258d061e926fd309d38d83922aaabd7f3
EBruce <git@ebruce.uk>
2021-06-17 17:26:48 +0100

    Update index.js

--- rs3/js/index.js
+++ rs3/js/index.js
@@ -2,8 +2,8 @@
 async function postData(url = './itemdb.json', data = {}) {
   // Default options are marked with *
   const response = await fetch(url, {
-    method: 'POST', // *GET, POST, PUT, DELETE, etc.
-    mode: 'cors', // no-cors, *cors, same-origin
+    method: 'GET', // *GET, POST, PUT, DELETE, etc.
+    mode: 'same-origin', // no-cors, *cors, same-origin
     cache: 'no-cache', // *default, no-cache, reload, force-cache, only-if-cached
     credentials: 'same-origin', // include, *same-origin, omit
     headers: {
