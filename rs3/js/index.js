b85d1e6ab1f2062e99e155b68dfdfc82da274752
EBruce <git@ebruce.uk>
2021-06-17 02:15:04 +0100

    -

--- rs3/js/index.js
+++ rs3/js/index.js
@@ -3,7 +3,6 @@ xmlhttp.onload = function() {
   const itemdata = JSON.parse(this.responseText);
   document.getElementById("price").innerHTML = itemdata.item.current.price;
 }
-xmlhttp.open("GET", "https://secure.runescape.com/m=itemdb_rs/api/catalogue/detail.json?item=440");
+xmlhttp.open("POST", "https://secure.runescape.com/m=itemdb_rs/api/catalogue/detail.json?item=440");
 xmlhttp.withCredentials = true;
-xmlhttp.responseType = 'json';
 xmlhttp.send();
